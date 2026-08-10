a0d5eea202c7b13236890e0b9fe6685718bfaf2c
Aaron-blip545 <princeaaronjamesb@gmail.com>
2026-08-10 18:06:58 +0800

    fuck

--- frontend-web/resources/js/pages/settings/password.tsx
+++ frontend-web/resources/js/pages/settings/password.tsx
@@ -1,26 +1,8 @@
+import HeadingSmall from '@/components/heading-small';
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-
-const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Password settings',
-        href: '/settings/password',
-    },
-];
-
-export default function Password() {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Password settings" />
-
-            <SettingsLayout>
-                <div />
-            </SettingsLayout>
-        </AppLayout>
-    );
-}
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
